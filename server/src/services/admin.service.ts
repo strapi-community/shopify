ae35a8a5797ab2b4bde8527b00b9e5c2be379a3c
Maksymilian Pamuła <mpamula@virtuslab.com>
2025-02-22 14:49:38 +0100

    feat: add services management and shop query validation

--- server/src/services/admin.service.ts
+++ server/src/services/admin.service.ts
@@ -1,3 +1,4 @@
+import { Core, UID } from '@strapi/strapi';
 import { omit, uniqBy } from 'lodash';
 import { StrapiContext } from '../@types';
 import { WebhookSubscriptionFormat } from '../@types/shopify';
@@ -5,8 +6,7 @@ import { BadRequestException } from '../exceptions/BadRequestException';
 import { getShopsRepository } from '../repositories/shop';
 import { getWebhookRepository } from '../repositories/webhook';
 import { getService } from '../utils/getService';
-import type { Settings, ShopifyShopWithId } from '../validators/admin.validator';
-import shopService from './shop.service';
+import { QueryShop, QueryShops, Settings, ShopifyShopWithId } from '../validators/admin.validator';
 
 const partialHideValue = (value: string) =>
   `${value.substring(0, 3)}*****${value.substring(value.length - 1)}`;
@@ -16,6 +16,25 @@ export default ({ strapi }: StrapiContext) => {
   const webhookRepository = getWebhookRepository(strapi);
   const webhookService = getService(strapi, 'webhook');
   const shopService = getService(strapi, 'shop');
+
+  const getMethodService = (strapi: Core.Strapi, name: UID.Service) => {
+    const service = strapi.service(name);
+    if (service.contentType) {
+      const standardStrapiMethods = [
+        'getDocumentId',
+        'find',
+        'createOrUpdate',
+        'findOne',
+        'create',
+        'update',
+        'delete',
+      ];
+      const serviceMethods = Object.keys(service);
+      return standardStrapiMethods.concat(serviceMethods)
+                        .filter((_: string) => typeof strapi.service(name as UID.Service)[_] === 'function');
+    }
+    return Object.keys(service);
+  };
 
   return {
     settings: {
@@ -47,42 +66,62 @@ export default ({ strapi }: StrapiContext) => {
       },
     },
     shops: {
-      async getShops() {
-        const shops = await shopsRepository.findMany();
+      async getShops(query: QueryShops) {
+        const shops = await shopsRepository.findMany({
+          where: { isActive: query.isActive },
+          populate: query.populate,
+        });
         return shops.map((shop) => ({
           ...shop,
           apiKey: partialHideValue(shop.apiKey),
           apiSecretKey: partialHideValue(shop.apiSecretKey),
         }));
       },
+      async getShop({ id, populate }: QueryShop) {
+        const shop = await shopsRepository.findOne({
+          where: { id },
+          populate,
+        });
+        if (!shop) {
+          return;
+        }
+        return {
+          ...shop,
+          apiKey: partialHideValue(shop.apiKey),
+          apiSecretKey: partialHideValue(shop.apiSecretKey),
+          adminApiAccessToken: partialHideValue(shop.adminApiAccessToken),
+        };
+      },
       async addShop(shop: ShopifyShopWithId) {
         const count = await shopsRepository.count({ where: { address: shop.address } });
         if (count > 0) {
           throw new BadRequestException('Shop already exists');
         }
-        const newShop = await shopsRepository.create(omit(shop, 'webhooks'));
-        const hooksData = await Promise.all(
-          uniqBy(shop.webhooks, 'topic').map((hook) =>
-            webhookRepository.create(true, {
-              ...hook,
-              format: WebhookSubscriptionFormat.Json,
-              shop: newShop,
-              service: hook.service,
-              method: hook.method,
-            })
-          )
-        );
-
-        const webhookData = await webhookService.create(shop.address, hooksData);
-        if (webhookData.length) {
-          await Promise.all(
-            webhookData.map((data) => webhookRepository.update({ id: data.id }, data))
+        return strapi.db.transaction(async () => {
+          const newShop = await shopsRepository.create(omit(shop, 'webhooks'));
+          const hooksData = await Promise.all(
+            uniqBy(shop.webhooks, 'topic').map((hook) =>
+              webhookRepository.create(true, {
+                ...hook,
+                format: WebhookSubscriptionFormat.Json,
+                shop: newShop,
+                service: hook.service,
+                method: hook.method,
+              }),
+            ),
           );
-        }
 
-        return shopsRepository.findOne({
-          where: { id: newShop.id },
-          populate: { webhooks: true },
+          const webhookData = await webhookService.create(shop.address, hooksData);
+          if (webhookData.length) {
+            await Promise.all(
+              webhookData.map((data) => webhookRepository.update({ id: data.id }, data)),
+            );
+          }
+
+          return shopsRepository.findOne({
+            where: { id: newShop.id },
+            populate: { webhooks: true },
+          });
         });
       },
       async removeShop(id: number) {
@@ -90,33 +129,35 @@ export default ({ strapi }: StrapiContext) => {
         if (!shop) {
           return;
         }
-        const result = await webhookService.remove(
-          shop.address,
-          shop.webhooks.map((hook) => hook.shopifyId)
-        );
+        return strapi.db.transaction(async () => {
+          const result = await webhookService.remove(
+            shop.address,
+            shop.webhooks.map((hook) => hook.shopifyId),
+          );
 
-        if (result.some((r) => r.hasError)) {
-          await Promise.all(
-            result
+          if (result.some((r) => r.hasError)) {
+            await Promise.all(
+              result
               .filter((r) => r.hasError)
-              .map((r) => webhookRepository.update({ shopifyId: r.id }, { errors: r.errors }))
-          );
-          throw new BadRequestException(
-            'Failed to remove shop',
-            result.map((r) => `The webhook ${r.id}: ${r.errors.join(', ')}`).join(', ')
-          );
-        }
-        const [removeResult] = await Promise.all([
-          shopsRepository.remove({ where: { id } }),
-          webhookRepository.remove({ where: { shop: { id } } }),
-          shopService.remove(shop.address),
-        ]);
-        return {
-          ...removeResult,
-          apiKey: partialHideValue(shop.apiKey),
-          apiSecretKey: partialHideValue(shop.apiSecretKey),
-          adminApiAccessToken: partialHideValue(shop.adminApiAccessToken),
-        };
+              .map((r) => webhookRepository.update({ shopifyId: r.id }, { errors: r.errors })),
+            );
+            throw new BadRequestException(
+              'Failed to remove shop',
+              result.map((r) => `The webhook ${r.id}: ${r.errors.join(', ')}`).join(', '),
+            );
+          }
+          const [removeResult] = await Promise.all([
+            shopsRepository.remove({ where: { id } }),
+            webhookRepository.remove({ where: { shop: { id } } }),
+            shopService.remove(shop.address),
+          ]);
+          return {
+            ...removeResult,
+            apiKey: partialHideValue(shop.apiKey),
+            apiSecretKey: partialHideValue(shop.apiSecretKey),
+            adminApiAccessToken: partialHideValue(shop.adminApiAccessToken),
+          };
+        });
       },
       async updateShop(newShop: ShopifyShopWithId) {
         throw new BadRequestException('Not implemented yet');
@@ -134,11 +175,41 @@ export default ({ strapi }: StrapiContext) => {
               ...hook,
               format: WebhookSubscriptionFormat.Json,
               shop: newShop,
-            })
-          )
+            }),
+          ),
         );
         return shopsRepository.update({ id: newShop.id }, newShop);
       },
     },
+    services: {
+      getServices() {
+        const services = strapi.services;
+        const blackList = [
+          'admin::',
+          'plugin::gql',
+          'plugin::i18n',
+          'plugin::email',
+          'plugin::upload',
+          'plugin::shopify',
+          'plugin::content-manager',
+          'plugin::content-type-builder',
+          'plugin::users-permissions',
+        ];
+        return Object.keys(services)
+                     .filter((name) => !blackList.some((_) => name.startsWith(_)))
+                     .map((name) => {
+                       try {
+                         return {
+                           name,
+                           methods: getMethodService(strapi, name as UID.Service),
+                         };
+                       } catch (e) {
+                         return { name, methods: [] };
+                       }
+                     })
+                     .filter((_) => _.methods.length > 0);
+      },
+    },
   };
 };
+
